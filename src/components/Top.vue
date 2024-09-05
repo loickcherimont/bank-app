4b438bb7e053a47fb78de0edbd315e9fbca4879a
loickcherimont <loickcherimont@gmail.com>
2024-09-05 22:27:15 +0200

    Explain <div>s roles

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -9,12 +9,13 @@ export default {
 
 <template>
     <header class='bg-slate-200 flex flex-col items-center p-5 gap-y-5 h-52'>
+      <!-- Account owner -->
       <h1 class='text-3xl font-thin'>{{ property }}</h1>
 
+      <!-- All available money actually -->
       <div class='subtitles flex justify-center items-center w-full h-full'>
         <h3 class='text-7xl font-bold'>{{ currentAccount.toFixed(2) }}€</h3>
       </div>
 
     </header>
 </template>
-<!-- <Top property='John Doe', :currentAccount="userMoney.toFixed(2)"/> -->
