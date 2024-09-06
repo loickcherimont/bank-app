b104c6f6ba29071e28daedfc86dffd1e894c55a4
loickcherimont <loickcherimont@gmail.com>
2024-09-06 17:52:36 +0200

    Create a toggle menu only on mobiles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    }
+  },
+
+  methods: {
+    /**
+     * Display or hide the menu
+     * for mobile
+     */
+    toggleMenu() {
+      this.isShow = !this.isShow;
+    }
+  }
+}
+</script>
 <template>
-  <!-- todo: Move into a toggle menu for mobiles -->
   <div>
-    <nav class='absolute right-0'>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/settings">Account Settings</RouterLink>
+    <nav class='absolute right-2 top-2'>
+      <!-- todo: Move toggle menu into @/components
+        replace this homemade tool by a real icon
+        use animation too
+      -->
+      <!-- Toggle menu appears only on mobiles -->
+      <div class="md:hidden flex flex-col items-center justify-center gap-y-1 border w-8 h-7 rounded"
+        @click="toggleMenu">
+        <span v-for="n in 3" class='border-b border-slate-300 h-0.5 w-1/2'></span>
+      </div>
+      <div :class="{ 'hidden': isShow }">
+        <div class='text-xs flex flex-col md:flex-row space-y-3 md:space-x-10 p-3' ref='menu'>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/settings">Account Settings</RouterLink>
+        </div>
+      </div>
+
+
     </nav>
     <RouterView />
   </div>
